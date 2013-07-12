c5782da6bb05f1298fbce3644982fc8382fad484
Fedor Nikolaev <fsqcds@gmail.com>
2013-07-12 14:59:01 +0400

    Votee v0.1

--- app.js
+++ app.js
@@ -14,14 +14,31 @@ app.config(function ($routeProvider) {
     })
 })
 
-app.controller('CreateCtrl', ['$scope', 'angularFire',
-  function CreateCtrl($scope, angularFire) {
-
+app.controller('CreateCtrl', ['$scope', '$location',
+  function CreateCtrl($scope, $location) {
+    var votesRef = new Firebase('https://voteebase.firebaseio.com/votes')
+    $scope.createVote = function() {
+      newVote = votesRef.push();
+      console.log(newVote);
+      console.log(newVote.name());
+      $location.path(newVote.name());
+    };
   }
 ]);
 
 app.controller('VoteCtrl', ['$scope', '$routeParams', 'angularFire',
   function CreateCtrl($scope, $routeParams, angularFire) {
-    $scope.id = $routeParams.id
+    var url = 'https://voteebase.firebaseio.com/votes/' + $routeParams.id;
+    var promise = angularFire(url, $scope, 'options', []);
+    promise.then(function() {
+      // Add a new item by simply modifying the model directly.
+      // Or, attach a function to $scope that will let a directive in markup manipulate the model.
+      $scope.addOption = function() {
+        $scope.options.push({name: $scope.text, score: 0});
+      };
+      $scope.addScore = function(option) {
+        option.score++;
+      }
+    });
   }
 ]);
